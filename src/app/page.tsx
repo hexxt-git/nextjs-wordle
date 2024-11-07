1c44753393697694ea5945811f163beda72b8795
HEXXT <88392191+hexxt-git@users.noreply.github.com>
2024-11-07 03:36:59 +0100

    wordle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,135 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState, useRef } from "react";
 
-export default function Home() {
+export default function GameBoard() {
+  const secret = 'svelte'
+  
+  enum KeyState {
+    Green = "green",
+    Yellow = "yellow",
+    Gray = "gray",
+    None = "none",
+  }
+
+  const backgroundMap: Map<KeyState, string> = new Map([
+    [KeyState.Green, "#538d4e"],
+    [KeyState.Yellow, "#b59f3b"],
+    [KeyState.Gray, "#3a3a3c"],
+    [KeyState.None, "transparent"],
+  ]);
+
+  interface Key {
+    key: string;
+    state: KeyState;
+  }
+
+  const [grid, setGrid]: [Array<Array<Key | null>>, any] = useState([]);
+  const [currentLine, setCurrentLine]: [Array<Key | null>, any] = useState([]);
+
+  const [keyboard, setKeyboard] = useState({
+    row1: "QWERTYUIOP".split(""),
+    row2: "ASDFGHJKL".split(""),
+    row3: "ZXCVBNM".split(""),
+  });
+
+  const containerRef = useRef<any>(null);
+  useEffect(() => {
+    if (containerRef.current) containerRef.current.focus();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen flex items-center justify-center flex-col text-center">
+      <h1 className="text-3xl uppercase font-semibold mb-6 tracking-widest">
+        Wordle
+      </h1>
+      <div
+        className="grid gap-2 text-xl outline-none"
+        onKeyDown={({ key }) => {
+          if (key === "Enter") {
+            if (currentLine.length < 5) return;
+            setGrid((prevGrid) => [
+              ...prevGrid,
+              currentLine.map((key, index) => {
+                if(!key) key = {key: 'a', state: KeyState.None}
+                
+                key.state = secret[index] === key.key ? KeyState.Green : secret.includes(key.key) ? KeyState.Yellow : KeyState.Gray;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                return key;
+              }),
+            ]);
+            setCurrentLine(() => []);
+          } else if (key === "Backspace") {
+            setCurrentLine((prev) => prev.slice(0, -1));
+          } else if (
+            key.trim().length == 1 &&
+            currentLine.length < 5 &&
+            grid.length < 6
+          ) {
+            setCurrentLine((prev) => [...prev, { key, state: KeyState.None }]);
+          }
+          console.log({ grid, currentLine });
+        }}
+        tabIndex={0}
+        ref={containerRef}
+      >
+        {[
+          ...grid,
+          grid.length <= 5 && [
+            ...currentLine,
+            ...Array(5 - currentLine.length).fill(null),
+          ],
+          grid.length < 5 && Array((5 - grid.length) * 5).fill(null),
+        ].map((row, rowIndex) => (
+          <div key={rowIndex} className="grid grid-cols-5 gap-2">
+            {row &&
+              row.map((cell: Key | null, index: number) => (
+                <div
+                  key={index}
+                  className="border-2 border-gray-200/20 rounded-lg w-16 h-16 flex items-center justify-center text-center"
+                  style={{
+                    background: backgroundMap.get(
+                      cell?.state ?? ("none" as KeyState.None)
+                    ),
+                  }}
+                >
+                  {cell?.key}
+                </div>
+              ))}
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-rows-3 mt-8">
+        <div className="flex gap-2 justify-center mt-2">
+          {keyboard.row1.map((key, index) => (
+            <button
+              key={index}
+              className="border-2 border-gray-200/20 rounded-lg min-w-12 h-12 flex items-center justify-center"
+            >
+              {key}
+            </button>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex gap-2 justify-center mt-2">
+          {keyboard.row2.map((key, index) => (
+            <button
+              key={index}
+              className="border-2 border-gray-200/20 rounded-lg min-w-12 h-12 flex items-center justify-center"
+            >
+              {key}
+            </button>
+          ))}
+        </div>
+        <div className="flex gap-2 justify-center mt-2">
+          {keyboard.row3.map((key, index) => (
+            <button
+              key={index}
+              className="border-2 border-gray-200/20 rounded-lg min-w-12 h-12 flex items-center justify-center"
+            >
+              {key}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
