3daf5f0d88b8212a2e0bcdcf3db6b2a41cc96ed8
HEXXT <88392191+hexxt-git@users.noreply.github.com>
2024-11-08 07:13:22 +0100

    keyboard indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,9 @@ export default function GameBoard() {
   const [currentLine, setCurrentLine]: [Array<Key | null>, any] = useState([]);
 
   const [keyboard, setKeyboard] = useState({
-    rows: ["QWERTYUIOP".split(""), "ASDFGHJKL".split(""), "ZXCVBNM".split("")],
+    keys: "qwertyuiopasdfghjklzxcvbnm"
+      .split("")
+      .map((key) => ({ key, state: KeyState.None })),
   });
 
   const containerRef = useRef<any>(null);
@@ -37,8 +39,62 @@ export default function GameBoard() {
     if (containerRef.current) containerRef.current.focus();
   }, []);
 
-  console.log({ grid, currentLine });
-  console.log({ secret });
+  function press(key: string) {
+    if (key === "Enter") {
+      if (currentLine.length < 5) return;
+      if (
+        !words.includes(currentLine.map((keyObj) => keyObj?.key ?? "").join(""))
+      )
+        return;
+      setGrid((prevGrid: Array<Array<Key | null>>) => [
+        ...prevGrid,
+        currentLine.map((key, index) => {
+          if (!key) key = { key: "a", state: KeyState.None };
+
+          key.state =
+            secret[index] === key.key
+              ? KeyState.Green
+              : [...secret].some(
+                  (character, index) =>
+                    character == key.key &&
+                    secret[index] !== currentLine[index]?.key
+                )
+              ? KeyState.Yellow
+              : KeyState.Gray;
+
+          const keyboardKey = keyboard.keys.find((x) => x.key == key.key);
+          if (keyboardKey) {
+            if (key.state === KeyState.Green)
+              keyboardKey.state = KeyState.Green;
+            else if (
+              key.state === KeyState.Yellow &&
+              keyboardKey.state !== KeyState.Green
+            )
+              keyboardKey.state = KeyState.Yellow;
+            else keyboardKey.state = KeyState.Gray;
+          } else {
+            console.error("used illegal key");
+          }
+
+          return key;
+        }),
+      ]);
+      setCurrentLine(() => []);
+    } else if (key === "Backspace") {
+      setCurrentLine((prev: Array<Key | null>) => prev.slice(0, -1));
+    } else if (
+      key.trim().length == 1 &&
+      currentLine.length < 5 &&
+      grid.length < 6
+    ) {
+      key = key.toLowerCase();
+      setCurrentLine((prev: Array<Key | null>) => [
+        ...prev,
+        { key, state: KeyState.None },
+      ]);
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center flex-col text-center">
       <h1 className="text-3xl uppercase font-semibold mb-6 tracking-widest">
@@ -46,48 +102,7 @@ export default function GameBoard() {
       </h1>
       <div
         className="grid gap-2 text-xl outline-none"
-        onKeyDown={({ key }) => {
-          if (key === "Enter") {
-            if (currentLine.length < 5) return;
-            if (
-              !words.includes(
-                currentLine.map((keyObj) => keyObj?.key ?? "").join("")
-              )
-            )
-              return;
-            setGrid((prevGrid: Array<Array<Key | null>>) => [
-              ...prevGrid,
-              currentLine.map((key, index) => {
-                if (!key) key = { key: "a", state: KeyState.None };
-
-                key.state =
-                  secret[index] === key.key
-                    ? KeyState.Green
-                    : [...secret].some(
-                        (character, index) =>
-                          character == key.key &&
-                          secret[index] !== currentLine[index]?.key
-                      )
-                    ? KeyState.Yellow
-                    : KeyState.Gray;
-
-                return key;
-              }),
-            ]);
-            setCurrentLine(() => []);
-          } else if (key === "Backspace") {
-            setCurrentLine((prev: Array<Key | null>) => prev.slice(0, -1));
-          } else if (
-            key.trim().length == 1 &&
-            currentLine.length < 5 &&
-            grid.length < 6
-          ) {
-            setCurrentLine((prev: Array<Key | null>) => [
-              ...prev,
-              { key, state: KeyState.None },
-            ]);
-          }
-        }}
+        onKeyDown={({ key }) => press(key)}
         tabIndex={0}
         ref={containerRef}
       >
@@ -118,14 +133,24 @@ export default function GameBoard() {
         ))}
       </div>
       <div className="grid grid-rows-3 mt-8">
-        {keyboard.rows.map((row) => (
+        {[
+          keyboard.keys.slice(0, 10),
+          keyboard.keys.slice(10, 19),
+          keyboard.keys.slice(19, 26),
+        ].map((row) => (
           <div className="flex gap-2 justify-center mt-2" key={row.join("")}>
             {row.map((key, index) => (
               <button
                 key={index}
-                className="border-2 border-gray-200/20 rounded-lg min-w-12 h-12 flex items-center justify-center"
+                className="border-2 border-gray-200/20 rounded-lg min-w-12 h-12 flex items-center justify-center uppercase"
+                style={{
+                  background: backgroundMap.get(
+                    key?.state ?? ("none" as KeyState.None)
+                  ),
+                }}
+                onClick={() => press(key.key)}
               >
-                {key}
+                {key.key}
               </button>
             ))}
           </div>
